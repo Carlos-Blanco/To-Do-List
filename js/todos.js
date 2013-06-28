3b4bff0dada919387a4ca3691fbfc938868e2f0a
Carlos Blanco <carlosblanco@me.com>
2013-06-29 00:28:21 +0200

    Add Flat UI to checkboxes

--- js/todos.js
+++ js/todos.js
@@ -6,7 +6,7 @@ function TodoCtrl($scope) {
 	};
 
 	$scope.addTodo = function(){
-		$scope.todos.push({text:$scope.formTodoText, done:false});
+		$scope.todos.push({text:$scope.formTodoText, done:false });
 		$scope.formTodoText = '';
 	};
 
